2117d88fc224164c2df9eed6dfae775d84902b14
USER <USER@LAPTOP-P7DK5BTP>
2020-03-01 20:21:28 +0530

    My first program in Hacker Rank

---
+++ HackerRank1.java
@@ -0,0 +1,31 @@
+package sbi.application.createAccount;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class HackerRank1 {
+	public static void main(String[] args) {
+		// Create a Scanner object to read input from stdin.
+		Scanner scan = new Scanner(System.in);
+		//Scanner sc = new Scanner(System.in);
+		// Read a full line of input from stdin and save it to our variable,
+		// inputString.
+		String inputString = scan.nextLine();
+		//String value = sc.nextLine();
+
+		// Close the scanner object, because we've finished reading
+		// all of the input from stdin needed for this challenge.
+		scan.close();
+		//sc.close();
+
+		// Print a string literal saying "Hello, World." to stdout.
+		System.out.println("Hello, World.");
+
+		// TODO: Write a line of code here that prints the contents of inputString to
+		// stdout.
+		System.out.println(inputString);
+	}
+}
